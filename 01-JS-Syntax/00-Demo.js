fe3d6c025b552959ee64b28b3a03073af2c73883
AsyaILIEVA <asyailieva9@gmail.com>
2026-07-02 17:52:45 +0100

    01-JS Syntax - 00-Demo

---
+++ 01-JS-Syntax/00-Demo.js
@@ -0,0 +1,120 @@
+// function demo(){
+//     let a = 5
+//     a = false
+//     console.log(a) //false 
+// }
+// demo()
+
+// function demo(){
+//     let a = 5
+//     let b = "test"
+//     console.log(a+b) //5test 
+// }
+// demo()
+
+// function demo(){
+//     let a = `5`
+    
+//     console.log(typeof a) //string 
+// }
+// demo()
+
+// function demo(){
+//     let a = 5
+//     let b = `5`
+    
+//     console.log(a === b) //false 
+
+//     // let result = a === b
+//     // console.log(result)
+
+// }
+// demo()
+
+// function demo(){
+//     let a = 5
+//     let b = `5`
+    
+//     let result = a === b
+
+//     if(result) {
+//         console.log(`two equal numbers`)
+//     } else{
+//         console.log(`two different numbers`)
+//     }    
+// }
+// demo()
+
+// function demo(){
+
+//     let name = `Ivo`
+
+//     switch(name) {
+//         case `Petq`: console.log(`Petq is here`); break;
+//         case `Ivo`: console.log(`Ivo is here`); break;
+//         default: console.log(`Person not found`); break;
+//     }
+// }
+// demo()
+
+// function demo() {
+
+//     let num = 5
+//     let num2 = 10
+
+//     if(num > 3 || num2 < 8) {
+//         console.log(`our number is > 3 or is < 8`)
+//     } // one of the statements is true
+// }
+// demo()
+
+// function demo() {
+
+//     let num = 5
+//     let num2 = 10
+
+//     if(num > 3 && num2 < 8) {
+//         console.log(`our number is > 3 or is < 8`) // all statements are true 
+//     } else {
+//         console.log(`false`)
+//     }
+// }
+// demo()
+
+// function demo() {
+
+//     let num2 = 10
+
+//     console.log(5**2)
+//     console.log(Math.pow(5,2));
+// }
+// demo()
+
+// function demo() {
+
+//     for (let a = 1; a <= 5; a++) {
+//         console.log(a)        
+//     }
+// }
+// demo()
+
+// function demo() {
+
+//     for (let a = 1; a <= 5; a++) {
+//         console.log(a)        
+//     }
+//     console.log(a)
+// }
+// demo()
+
+function demo() {
+
+    let a = 1;
+
+    while (a <= 5) {
+
+        console.log(a)
+        a++
+    }
+}
+demo()
