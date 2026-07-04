6f46a433fc85ad9e3419d908ae27ddec9b7f57cd
AsyaILIEVA <asyailieva9@gmail.com>
2026-07-05 00:06:13 +0100

    01-JS-Syntax-Ex05-Multiplication table

---
+++ 01-JS-Syntax/05-multiplicationTable.js
@@ -0,0 +1,8 @@
+function multiplicationTable(number) {
+
+    for (let currentNum = 1; currentNum <= 10; currentNum++) {
+        console.log(`${number} X ${currentNum} = ${number * currentNum}`)
+        
+    }
+}
+multiplicationTable(5)
