9e9da3511ede3deacd3ef9bd694a4f34dfba3c38
AsyaILIEVA <asyailieva9@gmail.com>
2026-07-11 00:05:41 +0100

    02-JS-Arrays-and-Strings-Ex03-List-of-Names-2 ways to print

--- 02-JS-Arrays-and-Strings/03-listOfNames.js
+++ 02-JS-Arrays-and-Strings/03-listOfNames.js
@@ -1,11 +1,15 @@
 function sortNames(arrNames) {
     
     let sortedNames = arrNames.sort((a, b) => a.localeCompare(b))
-    let arrayLength = sortNames.length
+    // let arrayLength = sortedNames.length
 
-    for (let i = 0; i < arrayLength; i++) {
+    // for (let i = 0; i < arrayLength; i++) {
         
-        console.log(`${i + 1}.${sortedNames[i]}`);
-    }
+    //     console.log(`${i + 1}.${sortedNames[i]}`);
+    // }
 
+    sortedNames.forEach((element, index) => {
+        console.log(`${index + 1}.${element}`)
+    });
 }
+sortNames(["Zack", "John", "Bob", "Ana"])
