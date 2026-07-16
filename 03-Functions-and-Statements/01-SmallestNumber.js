3359eefa28e7939bc218aaf790b32381612684ed
AsyaILIEVA <asyailieva9@gmail.com>
2026-07-16 14:33:45 +0100

    03-JS-Functions-and-Statements-Ex01-Smallest Number-var 2

--- 03-Functions-and-Statements/01-SmallestNumber.js
+++ 03-Functions-and-Statements/01-SmallestNumber.js
@@ -1,14 +1,16 @@
 function smallestNumber(num1, num2, num3) {
-    let smallestNumber = num1
+    
+    console.log(Math.min(num1, num2, num3))
+    // let smallestNumber = num1
 
-    if (smallestNumber > num2) {
-        smallestNumber = num2
-    }
+    // if (smallestNumber > num2) {
+    //     smallestNumber = num2
+    // }
 
-    if (smallestNumber > num3) {
-        smallestNumber = num3
-    }
+    // if (smallestNumber > num3) {
+    //     smallestNumber = num3
+    // }
 
-    console.log(smallestNumber)
+    // console.log(smallestNumber)
 } 
-smallestNumber(45, 5, 3)
+smallestNumber(1, 5, 3)
