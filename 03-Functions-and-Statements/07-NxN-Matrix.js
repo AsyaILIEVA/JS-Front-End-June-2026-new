f657db0054b66bd0ab32e86e327692d601b066fe
AsyaILIEVA <asyailieva9@gmail.com>
2026-07-17 20:55:25 +0100

    03-Functions-and-Statements-Ex07-1-Matrix

--- 03-Functions-and-Statements/07-NxN-Matrix.js
+++ 03-Functions-and-Statements/07-NxN-Matrix.js
@@ -4,8 +4,17 @@ function matrix(num) {
     }
 
     for (let currentRow = 1; currentRow <= num; currentRow++) {
-        
+
         console.log(rowGenerator(num).trimEnd())        
-    }    
+    } 
+    
+    // let result = ``
+    // for (let i = 1; i <= num;i++) {
+    //     for (let j = 1; j <= num; j++) {
+    //         result += num + ` `
+    //     }
+    //     console.log(result)
+    //     result = ``
+    // }
 }
 matrix(3)
