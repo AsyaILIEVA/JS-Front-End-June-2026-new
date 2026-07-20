3c4e3bd026f936e48d172e61cfcd3e25498457e6
AsyaILIEVA <asyailieva9@gmail.com>
2026-07-20 13:58:20 +0100

    04-JS-Objects-and-Classes-Ex01-Employees

---
+++ 04-Object-and-Classes/01-Employees.js
@@ -0,0 +1,17 @@
+function employees(names) {
+    
+    names.forEach(name => {
+        console.log(`Name: ${name} -- Personal Number: ${name.length}`)
+    })
+}
+employees([
+
+'Silas Butler',
+
+'Adnaan Buckley',
+
+'Juan Peterson',
+
+'Brendan Villarreal'
+
+])
