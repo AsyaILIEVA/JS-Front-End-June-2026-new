624b0642e446c0919999beaea57ece7b1138cab8
AsyaILIEVA <asyailieva9@gmail.com>
2026-07-23 13:31:17 +0100

    04-JS-Objects-and-Classes-demo

---
+++ 04-Object-and-Classes/00-demo.js
@@ -0,0 +1,16 @@
+let person = [`Pencho`, `Minchov`, 25, true];
+
+let personObj = { 
+    firstName: `Pencho`, 
+    lastName: `Minchov`, 
+    age: 25, 
+    // hasGraduated: true 
+};
+
+// console.log(personObj.age)
+// console.log(personObj[`age`]);
+
+personObj.age = 26
+personObj.hasGraduated = true
+
+console.log(personObj)
