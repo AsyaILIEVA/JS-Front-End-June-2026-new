583761c7ef6b73a4aea20e5c328bceeec0780fa9
AsyaILIEVA <asyailieva9@gmail.com>
2026-07-24 14:57:11 +0100

    04-JS-Object-and-Classes-00-demo

--- 04-Object-and-Classes/00-demo.js
+++ 04-Object-and-Classes/00-demo.js
@@ -5,6 +5,12 @@ let personObj = {
     lastName: `Minchov`, 
     age: 25, 
     // hasGraduated: true 
+    // sayHello: function() {
+    //     console.log(`Hi!`);
+    // }
+    sayHello: function(name) {
+        console.log(`Hi, ${name}`);
+    }
 };
 
 // console.log(personObj.age)
@@ -13,4 +19,15 @@ let personObj = {
 personObj.age = 26
 personObj.hasGraduated = true
 
-console.log(personObj)
+let personJson = JSON.stringify(personObj);
+console.log(personJson);
+
+let finalObj = JSON.parse(personJson)
+console.log(finalObj);
+
+// console.log(personObj)
+// console.log(personObj.sayHello());
+// personObj.sayHello(`Ginka`);
+
+// let keys = Object.keys(personObj);
+// console.log(keys);
