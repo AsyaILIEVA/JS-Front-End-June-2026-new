415e174cdb79851a671fb375b77b375411e8c1e0
AsyaILIEVA <asyailieva9@gmail.com>
2026-07-26 18:35:49 +0100

    05-JS-DOM Introduction-Ex02-Pascal-or-Camel-Case

--- 05-DOM-Indtroduction/02. Pascal or Camel Case/pascal-or-camel-case.js
+++ 05-DOM-Indtroduction/02. Pascal or Camel Case/pascal-or-camel-case.js
@@ -1,3 +1,21 @@
 function solve() {
-  //TODO
+  const userInput = document.getElementById(`text`).value.toLowerCase().split(` `)
+  const convention = document.getElementById(`naming-convention`).value 
+  const resultElement = document.getElementById(`result`)
+
+  const uppercaseWords = function (arr, index) {
+    return arr.slice(index).map(word => word[0].toUpperCase() + word.slice(1)).join(``)    
+  }
+
+  switch (convention) {
+    case `Camel Case`:
+      resultElement.textContent = userInput[0] + uppercaseWords(userInput, 1)
+      break;
+    case `Pascal Case`:
+      resultElement.textContent = uppercaseWords(userInput, 0)
+      break;
+    default:
+      resultElement.textContent = `Error!`;
+      
+  }
 }
