b36ac2f7fd3c8812b6bd3b7a905a503ba3a8a573
AsyaILIEVA <asyailieva9@gmail.com>
2026-07-26 19:22:51 +0100

    05-JS-DOM Introduction-Ex04-Search-in-List

--- 05-DOM-Indtroduction/04. Search in List/search-in-list.js
+++ 05-DOM-Indtroduction/04. Search in List/search-in-list.js
@@ -1,3 +1,23 @@
 function solve() {
-   // TODO
+   const listItems = document.querySelectorAll(`#towns li`)
+   const searchInput = document.getElementById(`searchText`).value 
+   const result = document.getElementById(`result`)
+
+   let matches = 0
+
+   listItems.forEach(li => {
+      const listItemLoweredText = (li.textContent).toLowerCase()
+      const searchLoweredText = searchInput.toLowerCase()
+
+      if (listItemLoweredText.includes(searchLoweredText)) {
+         li.style.fontWeight = `bold`
+         li.style.textDecoration = `underline`
+         matches++
+      } else {
+         li.style.fontWeight = ``
+         li.style.textDecoration = ``
+      }
+   })
+
+   result.textContent = `${matches} matches found`
 }
