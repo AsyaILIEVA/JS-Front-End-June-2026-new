16dfb2fcc25b3f75d7f982325d370bbc368652ed
AsyaILIEVA <asyailieva9@gmail.com>
2026-07-27 21:35:55 +0100

    05-JS-DOM Introduction-Ex05-Table-Search-Engine

--- 05-DOM-Indtroduction/05. Table - Search Engine/table-search-engine.js
+++ 05-DOM-Indtroduction/05. Table - Search Engine/table-search-engine.js
@@ -1,3 +1,18 @@
 function solve() {
-   //TODO
+   const studentList = document.querySelectorAll(`.container tbody tr`)
+   const searchTerm = document.getElementById(`searchField`).value.toLowerCase()
+
+   if (!(searchTerm !== ``)) return false
+
+   const students = Array.from(studentList).map(el => el.innerText)
+
+   studentList.forEach(item => {
+      item.classList.remove(`select`)
+   })
+
+   students.forEach((el, i) => {
+      if (students[i].toLowerCase().indexOf(searchTerm) >= 0) {
+         studentList[i].classList.add(`select`)
+      }
+   })   
 }
