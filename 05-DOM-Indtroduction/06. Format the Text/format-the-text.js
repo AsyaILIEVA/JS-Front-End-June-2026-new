68166d9fd4472eec90ed9fc0e339d8d6d3ad2c5a
AsyaILIEVA <asyailieva9@gmail.com>
2026-07-27 21:19:40 +0100

    05-JS-DOM Introduction-Ex06-Format-the-text

--- 05-DOM-Indtroduction/06. Format the Text/format-the-text.js
+++ 05-DOM-Indtroduction/06. Format the Text/format-the-text.js
@@ -1,3 +1,24 @@
 function solve() {
-  // TODO
+  const button = document.querySelector('button');
+  const input = document.getElementById('input');
+  const output = document.getElementById('output');
+
+  button.addEventListener('click', () => {
+    output.innerHTML = '';
+
+    const sentences = input.value
+      .split('.')
+      .filter(sentence => sentence.trim() !== '');
+
+      // Create paragraphs with up to 3 sentences each
+    for (let i = 0; i < sentences.length; i += 3) {
+      const paragraphText =
+        sentences
+        .slice(i, i + 3)
+        .map(s => s.trim() + '.')
+        .join('');
+
+      output.innerHTML += `<p>${paragraphText}</p>`;
+    }
+  });
 }
