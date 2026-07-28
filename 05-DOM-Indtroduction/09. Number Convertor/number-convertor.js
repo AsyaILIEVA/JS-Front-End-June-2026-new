a1dc3ba7b4e6c62408a05b594cc3673fb8c423fa
AsyaILIEVA <asyailieva9@gmail.com>
2026-07-28 20:39:45 +0100

    05-JS-DOM Introduction-Ex09-Number Convertor

--- 05-DOM-Indtroduction/09. Number Convertor/number-convertor.js
+++ 05-DOM-Indtroduction/09. Number Convertor/number-convertor.js
@@ -1,3 +1,33 @@
 function solve() {
-    // TODO
+    const input = document.getElementById('input');
+    const output = document.getElementById('result');
+    const select = document.getElementById('selectMenuTo');
+    const button = document.querySelector('button');
+
+    // Add options
+    const binaryOption = document.createElement('option');
+    binaryOption.value = 'binary';
+    binaryOption.textContent = 'Binary';
+
+    const hexOption = document.createElement('option');
+    hexOption.value = 'hexadecimal';
+    hexOption.textContent = 'Hexadecimal';
+
+    select.appendChild(binaryOption);
+    select.appendChild(hexOption);
+
+    button.addEventListener('click', convert);
+
+    function convert() {
+        const number = Number(input.value);
+        let result = '';
+
+        if (select.value === 'binary') {
+            result = number.toString(2);
+        } else if (select.value === 'hexadecimal') {
+            result = number.toString(16).toUpperCase();
+        }
+
+        output.value = result;
+    }
 }
