d81e45c292ac37e101c20e8f6e7706807e6c55b0
AsyaILIEVA <asyailieva9@gmail.com>
2026-07-29 21:48:14 +0100

    05-JS-DOM Introduction-L02-Collect List

--- 05-JS-Front-End-DOM-Introduction-Lab-Resources/02.Collect List Items/collect-list-items.js
+++ 05-JS-Front-End-DOM-Introduction-Lab-Resources/02.Collect List Items/collect-list-items.js
@@ -1,3 +1,9 @@
 function extractText() {
-    // TODO
+    let liEls = document.querySelectorAll(`li`)
+    let textareaEl = document.querySelector(`#result`)
+
+    for (let liEl of liEls) {
+        let text = liEl.textContent;
+        textareaEl.textContent += text + `\n`;
+    }
 }
