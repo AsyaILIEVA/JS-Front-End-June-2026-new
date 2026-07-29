f9191ffd5a52c408fe27d712b1bf58e30ce2aabe
AsyaILIEVA <asyailieva9@gmail.com>
2026-07-29 21:34:34 +0100

    05-JS-DOM Introduction-L01-Edit Element

--- 05-JS-Front-End-DOM-Introduction-Lab-Resources/01.Edit Element/edit-element.js
+++ 05-JS-Front-End-DOM-Introduction-Lab-Resources/01.Edit Element/edit-element.js
@@ -1,3 +1,6 @@
-function editElement() {
-    // TODO
+function editElement(htmlEl, strToFind, replacer) {
+    let originalText = htmlEl.textContent;
+    let updatedText = originalText.replaceAll(strToFind, replacer);
+
+    htmlEl.textContent = updatedText;
 }
