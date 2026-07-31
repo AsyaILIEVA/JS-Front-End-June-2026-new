d02fd2a068699ce3059c3aa63ac604c52f156307
AsyaILIEVA <asyailieva9@gmail.com>
2026-07-31 22:56:02 +0100

    06-JS-DOM Manipulation and Events-Ex01-Section

--- 06-DOM-Manipulation-and-Events/01. Sections/sections.js
+++ 06-DOM-Manipulation-and-Events/01. Sections/sections.js
@@ -1,5 +1,32 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-   // TODO
+   const formElement = document.getElementById('task-input');
+   const contentElement = document.getElementById('content');
+
+   formElement.addEventListener('submit', (e) => {
+      e.preventDefault();
+      contentElement.innerHTML = '';
+
+      const taskInput = document
+         .querySelector('input[type="text"]')
+         .value
+         .split(', ');
+
+      taskInput.forEach(element => {
+         const divEl = document.createElement('div');
+         const pEl = document.createElement('p');
+
+         pEl.textContent = element;
+         pEl.style.display = 'none';
+
+         divEl.append(pEl);
+
+         divEl.addEventListener('click', (e) => {
+            e.target.querySelector(`p`).style.display = 'block';
+         });
+
+         contentElement.append(divEl);
+      });
+   });
 }
