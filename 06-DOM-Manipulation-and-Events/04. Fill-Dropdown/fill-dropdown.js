f23832a9a1cba118cd039f4ee22e0062e9713a3c
AsyaILIEVA <asyailieva9@gmail.com>
2026-08-02 22:33:59 +0100

    06-JS-DOM-Ex04-Fill dropdown

--- 06-DOM-Manipulation-and-Events/04. Fill-Dropdown/fill-dropdown.js
+++ 06-DOM-Manipulation-and-Events/04. Fill-Dropdown/fill-dropdown.js
@@ -1,5 +1,26 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const menu = document.getElementById(`menu`)
+
+    document.querySelector(`form`).addEventListener(`submit`, (e) => {
+        e.preventDefault()
+
+        const newItemText = document.getElementById(`newItemText`).value 
+        const newItemValue = document.getElementById(`newItemValue`).value 
+
+        const areNotEmptyFields = newItemText !== `` && newItemValue !== ``
+
+        if(areNotEmptyFields) {
+            const newItem = document.createElement(`option`)
+            newItem.textContent = newItemText
+            newItem.setAttribute(`value`, newItemValue)
+
+            menu.append(newItem)
+
+            document.getElementById(`newItemText`).value = ``
+            document.getElementById(`newItemValue`).value = ``
+            document.getElementById(`newItemText`).focus()
+        }
+    })
 }
