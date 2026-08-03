ca25bf354b36f5a3e216ceec6678c333a9914e2d
AsyaILIEVA <asyailieva9@gmail.com>
2026-08-03 21:16:07 +0100

    06-JS-DOM-Ex05-Encode-and-decode-messages

--- 06-DOM-Manipulation-and-Events/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
+++ 06-DOM-Manipulation-and-Events/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
@@ -1,5 +1,31 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const buttons = document.querySelectorAll('button');
+    const textareas = document.querySelectorAll('textarea');
+
+    const sender = textareas[0];
+    const receiver = textareas[1];
+
+    buttons[0].addEventListener('click', encode);
+    buttons[1].addEventListener('click', decode);
+
+    function encode() {
+        const encoded = sender.value
+            .split('')
+            .map(ch => String.fromCharCode(ch.charCodeAt(0) + 1))
+            .join('');
+
+        receiver.value = encoded;
+        sender.value = '';
+    }
+
+    function decode() {
+        const decoded = receiver.value
+            .split('')
+            .map(ch => String.fromCharCode(ch.charCodeAt(0) - 1))
+            .join('');
+
+        receiver.value = decoded;
+    }
 }
